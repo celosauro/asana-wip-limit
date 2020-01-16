265e66a64f04e5b210d46607bc46d2dd924e9dc2
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2020-01-15 22:18:33 -0300

    init

---
+++ src/content.js
@@ -0,0 +1,48 @@
+const COLOR_RED = '#FC636B'
+const COLOR_YELLOW = '#FFB900'
+
+function getBoardColumns() {
+    return document.querySelectorAll('.BoardBody-columnDraggableItemWrapper')
+}
+
+function getHeaderName(column) {
+    return column.querySelector('.BoardColumnHeader-name').innerHTML || ''
+}
+
+function totalColumnCards(column) {
+    return column.querySelectorAll('.BoardCard').length
+}
+
+function getWipLimit(headerName) {
+    let wipRegex = (/\[\d+\]/g)
+    let wipLimit = wipRegex.exec(headerName)
+
+    if (wipLimit) {
+        return wipLimit[0].replace(/\D+/g, '')
+    }
+
+    return null
+}
+
+function hasWipLimit(headerName) {
+    return Boolean(getWipLimit(headerName))
+}
+
+function changeColumnBackgroundColor(column, color) {
+    column.style.backgroundColor = color
+}
+
+let columns = getBoardColumns()
+
+columns.forEach(function(column) {
+    let headerName = getHeaderName(column)
+    let totalCards = totalColumnCards(column)
+
+    if (hasWipLimit(headerName) && totalCards == getWipLimit(headerName)) {
+        changeColumnBackgroundColor(column, COLOR_YELLOW)
+    }
+    
+    if (hasWipLimit(headerName) && totalCards > getWipLimit(headerName)) {
+        changeColumnBackgroundColor(column, COLOR_RED)
+    }
+})
